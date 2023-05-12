699074fd581bf55db76b6425b40481566d33cff2
Vivian Ha <weha7612@uni.sydney.edu.au>
2023-05-13 03:34:27 +1000

    .

--- queue.c
+++ queue.c
@@ -55,6 +55,7 @@ order_node get_order_by_ids(int trader_id, int order_id) {
                     remove_order(curr, orderbook[i]);
                     return NULL;
                 }
+                printf("%d %s %d %d %d\n", curr->trader_id, curr->product, curr->order_id, curr->qty, curr->price);
                 return curr;
             }
             curr = curr->next;
